9300de4fcdc20e88ed109fe9dc709548943a95b0
Martin Průša <martin@martin-imac.localdomain>
2020-11-10 12:26:31 +0100

    dokonceni

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -22,6 +20,7 @@ public class Main {
         while (true) {
             boolean a = true;
             int plno = 0;
+            int plno2 = 0;
             for (int i = 0; i < ukoly.length; i++) {
                 if (ukoly[i] != null) {
                     plno++;
@@ -38,74 +37,150 @@ public class Main {
             System.out.println("----------------------------------------------------------------------------------------");
             System.out.println("1) Spravovat úkoly");
             System.out.println("2) Odejít");
-            int volba = sc.nextInt();
+            String strV = sc.nextLine();
+            String cisla = "0123456789";
+            boolean p = false;
+            int volba = 0;
+            for (int i = 0; i < strV.length(); i++) {
+                String x = "";
+                x += strV.charAt(i);
+                if (!cisla.contains(x)) {
+                    p = true;
+                }
+            }
+            if (!p) {
+                volba = Integer.parseInt(strV);
+            }
+
             if (volba == 1) {
                 while (a) {
                     System.out.println("----------------------------------------------------------------------------------------");
                     System.out.println("Správce úkolů");
+                    plno2 = 0;
+                    for (int i = 0; i < ukoly.length; i++) {
+                        if (ukoly[i] != null) {
+                            plno2++;
+                        }
+                    }
+                    System.out.println("----------------------------------------------------------------------------------------");
+                    System.out.println("Nyní můžete přidat 10 úkolů");
+                    System.out.println("Nyní máte " + plno2 + "/" + ukoly.length + " úkolů");
+                    for (int i = 0; i < ukoly.length; i++) {
+                        if (ukoly[i] != null) {
+                            System.out.println("#" + i + " - " + ukoly[i]);
+                        }
+                    }
                     System.out.println("----------------------------------------------------------------------------------------");
                     System.out.println("1) Vytvořit nový úkol");
-                    System.out.println("2) Upravit úkol");
-                    System.out.println("3) Smazat úkol");
+                    if (plno2 != 0) {
+                        System.out.println("2) Upravit úkol");
+                        System.out.println("3) Smazat úkol");
+                    }
                     System.out.println("4) Vrátit se do hlavního menu");
-                    volba = sc.nextInt();
+                    p = false;
+                    volba = 0;
+                    strV = sc.nextLine();
+                    for (int i = 0; i < strV.length(); i++) {
+                        String x = "";
+                        x += strV.charAt(i);
+                        if (!cisla.contains(x)) {
+                            p = true;
+                        }
+                    }
+                    if (!p) {
+                        volba = Integer.parseInt(strV);
+                    }
                     if (volba == 1) {
-                        a = false;
                         System.out.println("Zadej název úkolu: ");
                         for (int i = 0; i < ukoly.length; i++) {
                             if (ukoly[i] == null) {
-                                ukoly[i] = sc.nextLine();
                                 ukoly[i] = sc.nextLine();
                                 System.out.println(ukoly[i] + " byl uložen na index " + i);
                                 break;
                             }
                         }
                     } else if (volba == 2) {
-                        boolean b = true;
-                        while (b) {
-                            a = false;
-                            System.out.println("Zadej index na kterém chceš upravit úkol:");
-                            int index = sc.nextInt();
-                            if (index < -1 || index > 9) {
-                                System.out.println("Neplatné číslo. Zkus to znovu.");
-                            } else if (ukoly[index] == null) {
-                                System.out.println("Úkol na indexu " + index + " je prázdný");
-                            } else {
-                                System.out.println("Zadej upravený název úkolu: ");
-                                ukoly[index] = sc.nextLine();
-                                ukoly[index] = sc.nextLine();
-                                b = false;
+                        if (plno2 != 0) {
+                            boolean b = true;
+                            while (b) {
+                                System.out.println("Zadej index na kterém chceš upravit úkol:");
+                                int index = 0;
+                                p = false;
+                                strV = sc.nextLine();
+                                for (int i = 0; i < strV.length(); i++) {
+                                    String x = "";
+                                    x += strV.charAt(i);
+                                    if (!cisla.contains(x)) {
+                                        p = true;
+                                    }
+                                }
+                                if (!p) {
+                                    index = Integer.parseInt(strV);
+                                    if (index < -1 || index > 9) {
+                                        System.out.println("Neplatné číslo. Zkus to znovu.");
+                                    } else if (ukoly[index] == null) {
+                                        System.out.println("Úkol na indexu " + index + " je prázdný");
+                                    } else {
+                                        System.out.println("Zadej upravený název úkolu: ");
+                                        ukoly[index] = sc.nextLine();
+                                        b = false;
+                                    }
+                                } else{
+                                    System.out.println("Neplatná možnost");
+                                }
+
                             }
+                        } else {
+                            System.out.println("Neplatná možnost");
                         }
 
                     } else if (volba == 3) {
-                        boolean b = true;
-                        while (b) {
-                            a = false;
-                            System.out.println("Zadej index na kterém chceš smazat úkol: ");
-                            int index = sc.nextInt();
-                            if (index < -1 || index > 9) {
-                                System.out.println("Neplatné číslo. Zkus to znovu.");
-                            } else if (ukoly[index] == null) {
-                                System.out.println("Úkol na indexu " + index + " již je prázdný");
-                            } else {
-                                System.out.println("Zadej upravený název úkolu: ");
-                                System.out.println("Úkol "+ukoly[index]+" na indexu "+index+" byl vymazán.");
-                                ukoly[index] = null;
-                                b = false;
-                            }
-                            String[] ukoly2 = new String[ukoly.length];
-                            int pocet = 0;
-                            for(int i = 0; i< ukoly.length;i++){
-                                if(ukoly[i]!=null){
-                                    ukoly2[pocet] = ukoly[i];
-                                    pocet++;
+                        if (plno2 != 0) {
+                            boolean b = true;
+                            while (b) {
+                                System.out.println("Zadej index na kterém chceš smazat úkol: ");
+                                int index = 0;
+                                p = false;
+                                strV = sc.nextLine();
+                                for (int i = 0; i < strV.length(); i++) {
+                                    String x = "";
+                                    x += strV.charAt(i);
+                                    if (!cisla.contains(x)) {
+                                        p = true;
+                                    }
                                 }
+                                if (!p) {
+                                    index = Integer.parseInt(strV);
+                                    if (index < -1 || index > 9) {
+                                        System.out.println("Neplatné číslo. Zkus to znovu.");
+                                    } else if (ukoly[index] == null) {
+                                        System.out.println("Úkol na indexu " + index + " již je prázdný");
+                                    } else {
+                                        System.out.println("Zadej upravený název úkolu: ");
+                                        System.out.println("Úkol " + ukoly[index] + " na indexu " + index + " byl vymazán.");
+                                        ukoly[index] = null;
+                                        b = false;
+                                    }
+                                    String[] ukoly2 = new String[ukoly.length];
+                                    int pocet = 0;
+                                    for (int i = 0; i < ukoly.length; i++) {
+                                        if (ukoly[i] != null) {
+                                            ukoly2[pocet] = ukoly[i];
+                                            pocet++;
+                                        }
+                                    }
+                                    for (int i = 0; i < ukoly.length; i++) {
+                                        ukoly[i] = ukoly2[i];
+                                    }
+                                    plno2--;
+
+                                } else{
+                                    System.out.println("Neplatná možnost");
+                                }
+
                             }
-                            for(int i = 0; i< ukoly.length; i++){
-                                ukoly[i] = ukoly2[i];
-                            }
-                            System.out.println(Arrays.toString(ukoly));
+                        } else {
+                            System.out.println("Neplatná možnost");
                         }
                     } else if (volba == 4) {
                         a = false;
